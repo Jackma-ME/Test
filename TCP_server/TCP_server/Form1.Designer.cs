09f9aa7860a7fef88d818f35bc65d2cf6ae00319
Jackma-ME <40752331+Jackma-ME@users.noreply.github.com>
2018-08-23 15:26:28 +0800

    [Test]test

---
+++ TCP_server/TCP_server/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace TCP_server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgWorker_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.bgWorker_PC);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgWorker_RWC);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(28, 57);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(241, 148);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 12);
+            this.label1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 217);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ TCP_server/TCP_server/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCP_server
+{
+    public partial class Form1 : Form
+    {
+        //判斷UI狀態
+        int UI_status = 1;
+        //列舉判斷代碼, 方便辨識
+        enum E { waiting = 1, connected = 2, display = 3, disconnected = 4}
+        List<string> _Ls = new List<string>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getData()
+        {
+            TcpListener server = null;
+            try
+            {
+                //設置TCPlistener port 13000
+                Int32 port = 13000;
+                IPAddress localAddr = IPAddress.Parse("192.168.50.190");
+                //建立新的Tcplistener
+                server = new TcpListener(localAddr, port);
+                //server開始監聽client的請求
+                server.Start();
+                //建立Buffer來讀取資料
+                Byte[] bytes = new Byte[256];
+
+                string data = null;
+                //監聽迴圈
+                while (true)
+                {
+                    //設定背景作業能報告更新
+                    backgroundWorker1.WorkerReportsProgress = true;
+
+                    //傳入一個變數給ReportProgress來改變UI
+                    //無法直接在執行緒修改UI
+                    UI_status = Convert.ToInt32(E.waiting);   
+                    backgroundWorker1.ReportProgress(UI_status);
+
+                    //接收client的請求
+                    TcpClient client = server.AcceptTcpClient();
+
+                    UI_status = Convert.ToInt32(E.connected);
+                    backgroundWorker1.ReportProgress(UI_status);
+
+                    data = null;
+                    //取得client傳輸的資料
+                    NetworkStream stream = client.GetStream();
+
+                    //開始接收client的資料
+                    int i;
+                    while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+                        data = System.Text.Encoding.Unicode.GetString(bytes, 0, i);
+                        UI_status = Convert.ToInt32(E.display);
+                        if (data != "disconnect")
+                        {
+                            //將接收的資料存入 list
+                            _Ls.Add(data);
+                        }
+                        backgroundWorker1.ReportProgress(UI_status, data);
+                    }
+                    UI_status = Convert.ToInt32(E.disconnected);
+                    backgroundWorker1.ReportProgress(UI_status);
+                    client.Close();
+                }
+            }
+            catch (SocketException e)
+            {
+                MessageBox.Show("SocketException:{0}" + e);
+            }
+            //釋放區塊
+            finally
+            {
+                //sever停止監聽
+                server.Stop();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //開始執行背景作業
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void bgWorker_RWC(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("OK!");
+        }
+
+        private void bgWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            getData();
+        }
+
+        private void bgWorker_PC(object sender, ProgressChangedEventArgs e)
+        {
+            //此部分為看UI_status來改變UI
+            if (e.ProgressPercentage == (int)E.waiting)
+            {
+                label1.Text = "Waiting for connecting";
+            }
+            else if (e.ProgressPercentage == (int)E.connected)
+            {
+                if (e.ProgressPercentage != (int)E.disconnected)
+                {
+                    label2.Text = "Connected";
+                }
+            }
+            else if (e.ProgressPercentage == (int)E.display &&
+                e.UserState.ToString() != "disconnected")
+            {
+                richTextBox1.Text = "";
+                foreach(string data in _Ls)
+                {
+                    richTextBox1.Text += data;
+                }
+            }
+            if (e.ProgressPercentage == (int)E.disconnected)
+            {
+                label2.Text = "Disconnected!";
+            }
+        }
+    }
+}
